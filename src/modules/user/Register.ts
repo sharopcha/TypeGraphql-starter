a78b53d2c5745fdd0d248918b73eeea7a8d2e6e5
sharopcha <napster1202@gmail.com>
2022-01-31 02:10:47 +0200

    hash password

--- src/modules/user/Register.ts
+++ src/modules/user/Register.ts
@@ -1,30 +1,29 @@
-import { Arg,  Mutation, Query, Resolver,  } from "type-graphql";
-import  bcrypt from 'bcryptjs'
-import { User } from "../../entity/User";
+import { Arg, Mutation, Query, Resolver } from 'type-graphql';
+import bcrypt from 'bcryptjs';
+import { User } from '../../entity/User';
 
 @Resolver(User)
 export class RegisterResolver {
-  @Query(() => String, )
+  @Query(() => String)
   async hello() {
-    return 'Hello world';
+    return 'Hello world!';
   }
 
   @Mutation(() => User)
   async register(
-      @Arg('firstName') firstName: string,
-      @Arg('lastName') lastName: string,
-      @Arg('email') email: string,
-      @Arg('password') password: string,
-    ): Promise<User> {
-    
-        const hashedPassword =  await bcrypt.hash(password, 12);
+    @Arg('firstName') firstName: string,
+    @Arg('lastName') lastName: string,
+    @Arg('email') email: string,
+    @Arg('password') password: string
+  ): Promise<User> {
+    const hashedPassword = await bcrypt.hash(password, 12);
 
-        const user = await User.create({
-            firstName,
-            lastName,
-            email,
-            password: hashedPassword
-        }).save()
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    }).save();
 
     return user;
   }
