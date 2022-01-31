63d6797ec776130fb00710818b022a5108c223dc
sharopcha <napster1202@gmail.com>
2022-01-31 05:20:19 +0200

    login resolver and set cookie on header

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,53 @@
+import { LoginResolver } from './modules/user/Login';
 import 'reflect-metadata';
 import { ApolloServer } from 'apollo-server-express';
 import Express from 'express';
 import { buildSchema } from 'type-graphql';
 import { createConnection } from 'typeorm';
 import { RegisterResolver } from './modules/user/Register';
-
+import session from 'express-session';
+import connectRedis from 'connect-redis';
+import { redis } from './redis';
+import cors from 'cors';
 const main = async () => {
   await createConnection();
 
   const schema = await buildSchema({
-    resolvers: [RegisterResolver],
+    resolvers: [RegisterResolver, LoginResolver],
   });
 
-  const apolloServer = new ApolloServer({ schema });
+  const apolloServer = new ApolloServer({
+    schema,
+    context: ({ req }) => ({ req }),
+  });
 
   const app = Express();
+
+  const RedisStore = connectRedis(session);
+
+  app.use(
+    cors({
+      credentials: true,
+      origin: 'http://localhost:3000',
+    })
+  );
+
+  app.use(
+    session({
+      store: new RedisStore({
+        client: redis,
+      }),
+      name: 'qid',
+      resave: false,
+      saveUninitialized: false,
+      secret: 'hgsdjhgjhdsg',
+      cookie: {
+        secure: process.env.NODE_ENV === 'production', // if true only transmit cookie over https
+        httpOnly: true, // if true prevent client side JS from reading the cookie
+        maxAge: 1000 * 60 * 10, // session max age in miliseconds
+      },
+    })
+  );
 
   apolloServer.applyMiddleware({ app });
 
---
+++ src/modules/user/Login.ts
@@ -0,0 +1,32 @@
+import { MyContext } from '../../types/MyContext';
+import { Arg, Ctx, Mutation, Resolver } from 'type-graphql';
+import bcrypt from 'bcryptjs';
+import { User } from '../../entity/User';
+
+@Resolver(User)
+export class LoginResolver {
+  @Mutation(() => User, { nullable: true })
+  async login(
+    @Arg('email') email: string,
+    @Arg('password') password: string,
+    @Ctx() ctx: MyContext
+  ): Promise<User | null> {
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error('User does not exist');
+    }
+
+    const valid = bcrypt.compare(password, user.password);
+
+    if (!valid) {
+      throw new Error('Invalid credentials');
+    }
+
+    ctx.req.session.userId = user.id;
+
+    console.log(ctx.req.session!.userId);
+
+    return user;
+  }
+}
